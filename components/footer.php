e979b0dae2a416c7c3fea749b80409f656b7e321
BensonKas1508 <benson.vorsah@ashesi.edu.gh>
2025-11-28 10:51:29 +0000

    made some tweeks

--- components/footer.php
+++ components/footer.php
@@ -21,3 +21,56 @@
 
     <p class="copy">© <?= date('Y') ?> Thoughtful. All rights reserved.</p>
 </footer>
+
+<!-- Footer -->
+  <footer class="footer">
+    <div class="footer-container">
+      <div class="footer-column">
+        <h3>Thoughtful</h3>
+        <p>Making gifting easier with thoughtfully curated gifts for every occasion.</p>
+        <div class="social-links">
+          <a href="#"><i class="fab fa-facebook-f"></i></a>
+          <a href="#"><i class="fab fa-twitter"></i></a>
+          <a href="#"><i class="fab fa-instagram"></i></a>
+          <a href="#"><i class="fab fa-pinterest"></i></a>
+        </div>
+      </div>
+      
+      <div class="footer-column">
+        <h3>Shop</h3>
+        <ul>
+          <li><a href="/products.php">All Gifts</a></li>
+          <li><a href="/occasions">By Occasion</a></li>
+          <li><a href="/budget">By Budget</a></li>
+          <li><a href="/vendors">Vendors</a></li>
+          <li><a href="/categories.php">Categories</a></li>
+        </ul>
+      </div>
+      
+      <div class="footer-column">
+        <h3>Help</h3>
+        <ul>
+          <li><a href="/contact.php">Contact Us</a></li>
+          <li><a href="/faq">FAQ</a></li>
+          <li><a href="/shipping">Shipping Info</a></li>
+          <li><a href="/returns">Returns</a></li>
+          <li><a href="/track-order">Track Order</a></li>
+        </ul>
+      </div>
+      
+      <div class="footer-column">
+        <h3>Company</h3>
+        <ul>
+          <li><a href="/about">About Us</a></li>
+          <li><a href="/careers">Careers</a></li>
+          <li><a href="/press">Press</a></li>
+          <li><a href="/blog">Blog</a></li>
+          <li><a href="/affiliates">Affiliates</a></li>
+        </ul>
+      </div>
+    </div>
+    
+    <div class="copyright">
+      <p>&copy; 2025 ThoughtfulGift.com | <a href="/privacy-policy">Privacy Policy</a> | <a href="/terms-of-use">Terms of Use</a></p>
+    </div>
+  </footer>
