f23d8c33d9f884f1081270ac90baebe3f77643f0
BensonKas1508 <benson.vorsah@ashesi.edu.gh>
2025-11-30 23:22:17 +0000

    navbar update

--- components/navbar.php
+++ components/navbar.php
@@ -89,6 +89,16 @@ if (session_status() === PHP_SESSION_NONE) {
                             </svg>
                             My Orders
                         </a>
+
+                        <?php if ($_SESSION['role'] === 'admin'): ?>
+                            <a href="admin/dashboard.php" class="dropdown-item">
+                                <svg width="16" height="16" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M3 4a1 1 0 011-1h12a1 1 0 011 1v2a1 1 0 01-1 1H4a1 1 0 01-1-1V4zM3 10a1 1 0 011-1h6a1 1 0 011 1v6a1 1 0 01-1 1H4a1 1 0 01-1-1v-6zM14 9a1 1 0 00-1 1v6a1 1 0 001 1h2a1 1 0 001-1v-6a1 1 0 00-1-1h-2z"/>
+                                </svg>
+                                Admin Dashboard
+                            </a>
+                        <?php endif; ?>
+
                         <div class="dropdown-divider"></div>
                         <a href="logout.php" class="dropdown-item logout-item">
                             <svg width="16" height="16" viewBox="0 0 20 20" fill="currentColor">
